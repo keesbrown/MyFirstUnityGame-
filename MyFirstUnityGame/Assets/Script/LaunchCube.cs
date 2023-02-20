3b8738c96a767a87fa4a2eb7ccb9ae7f1469cc6f
unknown <kees.brown1@gmail.com>
2023-02-20 12:13:38 +0100

    launch cube function er in gezet

---
+++ MyFirstUnityGame/Assets/Script/LaunchCube.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchCube : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+        }
+    }
+}
